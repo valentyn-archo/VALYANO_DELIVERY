83e72a2f9fef6a5018077595630ccdb611a1202f
vvyshyvan <vyshyvanv.official@gmail.com>
2021-11-25 21:34:32 +0200

    add some form fields, validation and install two npm packages

--- src/components/Pages/Cart/CartPage/CartPage.jsx
+++ src/components/Pages/Cart/CartPage/CartPage.jsx
@@ -1,5 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
+import { useForm } from 'react-hook-form';
 import { Modal } from 'react-responsive-modal';
+import { isPossiblePhoneNumber } from "react-phone-number-input";
+import PhoneInputWithCountry from "react-phone-number-input/react-hook-form";
 
 import { Context } from "utils/context";
 import { LINK_NEW_ORDER, POST_DATA } from "utils/consts";
@@ -14,6 +17,7 @@ import Loader from "components/Loader";
 import classes from "./CartPage.module.scss";
 
 import 'react-responsive-modal/styles.css';
+import 'react-phone-number-input/style.css';
 
 const CartPage = () => {
   const {
@@ -22,15 +26,28 @@ const CartPage = () => {
     loading,
   } = useContext(Context);
 
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+
   const [openModal, setOpenModal] = useState(false);
+  const [value, setValue] = useState('');
 
   const totalPrice = cartProductList.length ? cartProductList
     .reduce((sum, item) => sum + item.price * item.amount, 0)
     .toFixed(2) : 0;
 
-  // eslint-disable-next-line no-unused-vars
   const onHandlerCheckout = () =>
     fetching(POST_DATA, LINK_NEW_ORDER, { cartProductList, totalPrice });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    setOpenModal(false);
+    onHandlerCheckout();
+  }
 
   useEffect(() => setTitle("Cart"), []);
 
@@ -51,6 +68,25 @@ const CartPage = () => {
             }}
           >
             Here you need to provide any information...
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <PhoneInputWithCountry
+                international
+                countryCallingCodeEditable={false}
+                defaultCountry="UA"
+                value={value}
+                onChange={setValue}
+                className={classes.phoneInput}
+                control={control}
+                name="phoneInput"
+                rules = {{ required: true, validate: isPossiblePhoneNumber }}
+              />
+              {errors.phoneInput && <p style={{"color": "red"}}>Please enter correct phone number.</p>}
+              <input className={classes.textInput} {...register('name', { required: true })} />
+              {errors.name && <p style={{"color": "red"}}>Please enter name. It's required.</p>}
+              <input className={classes.textInput} {...register('address', { required: true })} />
+              {errors.address && <p style={{"color": "red"}}>Please enter address for delivery.</p>}
+              <input type="submit" />
+            </form>
           </Modal>
           {cartProductList.map((productItem) => {
             return <ProductInCart key={productItem._id} item={productItem} />;
@@ -60,7 +96,7 @@ const CartPage = () => {
             <Icons icon={"price"} />
             <p>{totalPrice}</p>
           </div>
-          <div className={classes.checkout} onClick={() => setOpenModal(true)} /*onClick={onHandlerCheckout} */>
+          <div className={classes.checkout} onClick={() => setOpenModal(true)}>
             <Button>Checkout</Button>
           </div>
         </div>
